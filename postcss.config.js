1d5e5c15a57e37abb2b247c02a4a486e2a05460c
vladimir858 <standardwebsites@gmail.com>
2025-12-05 22:49:17 +0300

    new file:   .gitignore 	new file:   minista.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   src/jsconfig.json 	new file:   src/pages/index.jsx

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+import postcssPxToRem from 'postcss-pxtorem'
+export default ({ env }) => {
+ const isProd = env === "production"
+  const plugins = []
+  if (isProd){
+    plugins.push(
+        postcssPxToRem({
+            prodList: ['*'],
+            mediaQuery: true,
+        })
+    )
+  }
+  return {
+    plugins,
+  }
+}
