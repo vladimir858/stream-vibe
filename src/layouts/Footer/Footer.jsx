be7d317532baa006dbd6cdc1bb7816140f2ed28c
vladimir858 <standardwebsites@gmail.com>
2026-03-26 18:56:00 +0300

    add footer scss

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -5,82 +5,89 @@ const Footer = () => {
   const menuItems =
     [
       {
-    title: 'Home',
-    links: ['Categories', 'Devices', 'Pricing', 'FAQ'],
-    href: '/',
-  },
+        title: 'Home',
+        links: ['Categories', 'Devices', 'Pricing', 'FAQ'],
+        href: '/',
+      },
       {
-    title: 'Movies',
-    links: ['Gernes', 'Trending', 'New Release', 'Popular'],
-    href: 'Movies',
-  }, {
-    title: 'Shows',
-    links: ['Gernes', 'Trending', 'New Release', 'Popular'],
-    href: 'Shows',
-  }, {
-    title: 'Support', links: ['Contact Us'], href: 'Contact Us'
-  }, {
-    title: 'Subscription', links: ['Plans', 'Features'], href: 'Subscription',
-  }, {
-    title: 'Connect With Us', href: 'connect-with-us', socialLinks: [{
-      label: 'Facebook', iconName: 'facebook',
+        title: 'Movies',
+        links: ['Gernes', 'Trending', 'New Release', 'Popular'],
+        href: 'Movies',
+      }, {
+      title: 'Shows',
+      links: ['Gernes', 'Trending', 'New Release', 'Popular'],
+      href: 'Shows',
     }, {
-      label: 'Twitter', iconName: 'twitter',
+      title: 'Support', links: ['Contact Us'], href: 'Contact Us'
     }, {
-      label: 'LinkedIn', iconName: 'linked-in',
+      title: 'Subscription', links: ['Plans', 'Features'], href: 'Subscription',
+    }, {
+      title: 'Connect With Us', href: 'connect-with-us', socialLinks: [{
+        label: 'Facebook', iconName: 'facebook',
+      }, {
+        label: 'Twitter', iconName: 'twitter',
+      }, {
+        label: 'LinkedIn', iconName: 'linked-in',
+      },]
     },]
-  },]
 
-  const  extraLinks = ['Text Container', 'Privacy Policy' , 'Cookie Policy']
+  const extraLinks = ['Text Container', 'Privacy Policy', 'Cookie Policy']
 
-  return (<footer className="footer">
-      <div className="footer__inner container">
-        <nav className="footer__menu">
-          {menuItems.map(({title, links, socialLinks, href}, index) => (<div
-              className="footer__menu-column"
+  return (
+    <footer className="footer">
+    <div className="footer__inner container">
+      <nav className="footer__menu">
+        {menuItems.map(({title, links, socialLinks, href}, index) => (<div
+          className="footer__menu-column"
+          key={index}
+        >
+          <a
+            className="footer__menu-title h6"
+            href={href}
+          >
+            {title}
+          </a>
+          {links?.length > 0 && (<ul className='footer__menu-list'>
+            {links.map((link, index) => (<li
+              className="footer__menu-item"
               key={index}
             >
               <a
-                className="footer__menu-title h6"
-                href={href}
+                className="footer__menu-link"
+                href="/"
               >
-                {title}
-              </a>
-              {links?.length > 0 && (<ul className='footer__menu-list'>
-                  {links.map((link, index) => (<li
-                      className="footer__menu-item"
-                      key={index}
-                    >
-                      <a
-                        className="footer__menu-link"
-                        href="/"
-                      >
-                        {link}
-                      </a>
-                    </li>))}
-                </ul>)}
-              {socialLinks?.length > 0 && (
-                <Socials
-                  className="footer__soc1als"
-                  links={socialLinks}
-                />
-              )}
-            </div>))}
-        </nav>
-        <div className="footer__extra">
-          <p className="footer__copyright">
-            @<time dataTime="2023"> 2023</time> streamvib, All Rights Reserved
-          </p>
-          <div className="footer__extra-links">
-            {extraLinks.map((link, index) => (
-              <a className="footer__extra-link" key={index}  href="/">
                 {link}
               </a>
-            ))}
-          </div>
+            </li>))}
+          </ul>)}
+          {socialLinks?.length > 0 && (
+            <Socials
+              className="footer__soc1als"
+              links={socialLinks}
+            />
+          )}
+        </div>))}
+      </nav>
+      <div className="footer__extra">
+        <p className="footer__copyright">
+          @
+          <time dataTime="2023"> 2023</time>
+          streamvib, All Rights Reserved
+        </p>
+        <div className="footer__extra-links">
+          {extraLinks.map((link, index) => (
+            <a
+              className="footer__extra-link"
+              key={index}
+              href="/"
+            >
+              {link}
+            </a>
+          ))}
         </div>
       </div>
-    </footer>)
+    </div>
+  </footer>)
 }
 
 export default Footer
