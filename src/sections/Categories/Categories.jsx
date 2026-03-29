c1d7fc0db134b1ce6ae6674c60e98266203ffa92
vladimir858 <standardwebsites@gmail.com>
2026-03-29 17:56:31 +0300

    feat movies slider

---
+++ src/sections/Categories/Categories.jsx
@@ -0,0 +1,38 @@
+import Section from '@/layouts/Section'
+import CategoryCard from '@/components/CategoryCard'
+import Slider from '@/components/Slider'
+import SliderNavigation from '@/components/Slider/components/SliderNavigation'
+import categoryItems from './categoryItems'
+
+const Categories = () => {
+  const sliderNavigationId = 'categories-slider-navigation'
+
+  return (
+    <Section
+      title="Explore our wide variety of categories"
+      titleId="categories-title"
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={(
+        <SliderNavigation
+          mode="tile"
+          id={sliderNavigationId}
+        />
+      )}
+      isActionsHiddenOnMobile
+    >
+      <Slider
+        navigationTargetElementId={sliderNavigationId}
+        isBeyondTheViewportOnMobileS
+      >
+        {categoryItems.map((categoryItem, index) => (
+          <CategoryCard
+            {...categoryItem}
+            key={index}
+          />
+        ))}
+      </Slider>
+    </Section>
+  )
+}
+
+export default Categories
