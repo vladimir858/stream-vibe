51c596e77dd5cdc757503c1232bee648f040d707
vladimir858 <standardwebsites@gmail.com>
2026-04-19 09:59:50 +0300

    Add Show pages

---
+++ src/pages/show.jsx
@@ -0,0 +1,14 @@
+import ShowBanner from "@/sections/ShowBanner";
+
+export const metadata = {
+  title: 'Shows -  Stranger Things ',
+}
+
+export default function () {
+  return (
+    <>
+
+      <ShowBanner />
+    </>
+  )
+}
